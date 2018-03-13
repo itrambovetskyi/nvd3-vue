6264b7bacc841c943a82ae1605bd166b3f9fa10d
itrambovetskiy <igortram@gmail.com>
2018-03-13 10:10:37 +0200

    Line chart configuration

--- client/src/components/LoadChart.vue
+++ client/src/components/LoadChart.vue
@@ -1,37 +1,53 @@
 <template>
-    <line-chart :chart-data="chartData"></line-chart>
+    <line-chart :chart-data="chartData"
+                :options="chartOptions">
+    </line-chart>
 </template>
 
 <script>
     import LineChart from './charts/LineChart';
 
     const chartData = [{
-        key: "Cosine Wave",
+        key: "Sinus Wave",
         color: "#2ca02c",
         values: []
+    }, {
+        key: "Cosine Wave",
+        color: "#ffa02c",
+        values: []
     }];
+
+    const chartOptions = {
+        width: 800,
+        height: 400
+    };
 
     let counter = 0;
     setInterval(() => {
         chartData[0].values.push({
             x: counter,
-            //y: Math.sin(counter / (100 / (Math.PI*2))) + Math.random() * 0.2
-            y: Math.random()
+            y: Math.sin(counter / (35 / (Math.PI*2)))
+        });
+        chartData[1].values.push({
+            x: counter,
+            y: Math.cos(counter / (35 / (Math.PI*2)))
         });
 
-        if (counter > 200) {
+        if (counter > 70) {
             chartData[0].values.shift();
+            chartData[1].values.shift();
         }
 
         counter++;
-    }, 100);
+    }, 500);
 
     export default {
         name: 'LoadChart',
         components: {LineChart},
         data() {
             return {
-                chartData: chartData
+                chartData: chartData,
+                chartOptions: chartOptions
             }
         },
         computed: {},
--- client/src/components/charts/LineChart.vue
+++ client/src/components/charts/LineChart.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :id="id" :width="width" :height="height">
+    <div :id="id" :width="options.width" :height="options.height">
         <svg></svg>
     </div>
 </template>
@@ -10,6 +10,37 @@
     import shortId from 'shortid';
     import _ from 'lodash';
 
+    const defaultOptions = {
+        duration: 0,
+        useInteractiveGuideline: true,
+        isArea: false,
+        width: 600,
+        height: 300,
+        showLabels: true,
+        showLegend: true,
+        rightAlignYAxis: false,
+        showVoronoi: false,
+        showXAxis: true,
+        showYAxis: true,
+        legendPosition: `top`,
+        margin: {
+            top: 30,
+            right: 20,
+            bottom: 50,
+            left: 60
+        },
+        noData: `No Data Available.`,
+        useVoronoi: true
+    };
+    const defaultXAxis = {
+        label: `X Axis Label`,
+        format: d3.format(',.1f')
+    };
+    const defaultYAxis = {
+        label: `Y Axis Label`,
+        format: d3.format(',.1f')
+    };
+
     let chart;
 
     export default {
@@ -17,37 +48,20 @@
         props: {
             chartData: {
                 type: Array,
-                default: () => ([])
+                default: () => []
             },
             options: {
                 type: Object,
-                default: () => ({
-                    duration: 0,
-                    useInteractiveGuideline: true
-                })
+                default: () => defaultOptions
             },
             xAxis: {
                 type: Object,
-                default: () => ({
-                    label: `X Axis Label`,
-                    format: d3.format(',.1f')
-                })
+                default: () => defaultXAxis
             },
             yAxis: {
                 type: Object,
-                default: () => ({
-                    label: `Y Axis Label`,
-                    format: d3.format(',.1f')
-                })
-            },
-            height: {
-                type: Number,
-                default: 300
-            },
-            width: {
-                type: Number,
-                default: 600
-            },
+                default: () => defaultYAxis
+            }
         },
         data() {
             return {
@@ -57,24 +71,35 @@
         mounted() {
             const me = this;
 
-            nv.addGraph(() => {
-                chart = nv.models.lineChart().options(me.options);
+            const options = _.merge(defaultOptions, me.options);
+            const xAxis = _.merge(defaultXAxis, me.xAxis);
+            const yAxis = _.merge(defaultYAxis, me.yAxis);
 
-                chart.width(me.width);
-                chart.height(me.height);
+            nv.addGraph(() => {
+                chart = nv.models.lineChart().options(options);
 
                 chart.xAxis
-                    .axisLabel(me.xAxis.label)
-                    .tickFormat(me.xAxis.format)
-                    .staggerLabels(true);
+                    .axisLabel(xAxis.label)
+                    .axisLabelDistance()
+//                    .domain()
+//                    .duration()
+//                    .fontSize()
+//                    .height()
+//                    .margin()
+//                    .orient()
+//                    .range()
+//                    .rangeBand()
+//                    .rangeBands()
+//                    .rotateLabels()
+//                    .
+                ;
 
                 chart.yAxis
-                    .axisLabel(me.yAxis.label)
-                    .tickFormat(me.yAxis.format);
+                    .axisLabel(yAxis.label);
 
                 d3.select(`#${me.id} svg`)
-                    .style('height', me.height)
-                    .style('width', me.width)
+                    .style('height', options.height)
+                    .style('width', options.width)
                     .datum(me.chartData)
                     .call(chart);
 
@@ -86,7 +111,11 @@
         computed: {},
         watch: {
             chartData: {
-                handler: _.debounce(() => chart.update(), 50),
+                handler: _.debounce(() => chart.update(), 100),
+                deep: true
+            },
+            options: {
+                handler: _.debounce(() => chart.update(), 100),
                 deep: true
             }
         }
